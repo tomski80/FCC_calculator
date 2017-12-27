d904992b94747760a7063cc0c3fba45a09b595f1
tom.straszewski <tomek.straszewski@gmail.com>
2017-12-27 12:13:58 +0000

    final css, renamed javascripts

--- js/calculator.js
+++ js/calculator.js
@@ -1,218 +1,175 @@
-
 'use strict'
 
-$( function (){
+const MAX_LENGTH = 10;
 
+$( function(){
 
     let calc = {
-        display : '0',
+        result : [],
+        display: '0',
         number : [],
-        operationChain : [],
-        result : 0,
-        sign : 1,
-        numberEntered : true,
+        operand : null,
+        enterNumber : true,
 
         updateDisplay: function(){
+            this.display = +this.display;
             $('.display').val(this.display);
         },
 
         concatDigit: function(btn){
-            this.numberEntered = true;
-            let digit = $(btn).val();
-            this.number.push(digit);
-            //this.number.length ? this.display = this.number.join('') : this.number.push('0');
-            this.display = this.number.join('');
+            if(this.operand != '=' && this.number.length < MAX_LENGTH){
+                let digit = $(btn).val();
+                this.number.push(digit);
+                this.display = this.number.join('');
+                this.updateDisplay();
+            }
+        },
+
+        addDecimal: function(){
+            if(!(this.number.includes('.'))){
+                if(this.number.length){
+                    this.number.push('.');
+                }else{
+                    this.number.push('0');
+                    this.number.push('.');
+                }
+            }
+        },
+
+        clearNum: function(){
+            if(this.number.length){
+                this.number = [];
+                this.display = '0';
+                this.updateDisplay();
+            }
+        },
+
+        clearAll: function(){
+            this.result = [];
+            this.number = [];
+            this.display = '0';
+            this.operand = null;
             this.updateDisplay();
         },
 
-        pushNumber: function(){
-            if(!this.number.length){
-                this.number = ['0'];
-            }
-            let number = this.number.join('');
-            this.operationChain.push(number);
-            this.number = [];
-            this.numberEntered = false;
-        },
-
-        pushOperation: function(operand){
-            let lastIndex = this.operationChain.length - 1,
-            lastElem  = this.operationChain[lastIndex];
-            if( ['*','/','+','-','='].includes(lastElem) ){
-                this.operationChain[lastIndex] = operand;
+        percent: function(){
+            if((this.operand === '*') || (this.operand === '/')){
+                let number = +this.number.join('');
+                number = number / 100;
+                this.number = [];
+                this.number.push(number);
+                this.execute('=');
             }else{
-                this.operationChain.push(operand);
+                let number = +this.number.join('');
+                if(this.result.length && this.number.length){
+                    number = this.result * number / 100;
+                    this.number = [];
+                    this.number.push(number);
+                    this.execute('=');
+                }else if(this.number.length){
+                    number = number / 100;
+                    this.result.push(number);
+                    this.display = this.result[0];
+                    this.number = [];
+                    this.execute('=');
+                    this.updateDisplay();
+                }
             }
-        },
-
-        updateOperationChain: function(operand){
-            if(this.numberEntered){
-                this.pushNumber();
-            }
-            this.pushOperation(operand);
-            console.log('chain:'+this.operationChain);
-            this.eval();
-        },
-
-        percentage: function(){
-            let last = this.operationChain.length - 1;
-            if(['*','/','+','-','='].includes(this.operationChain[last])){
-                this.operationChain.pop();  //remove operation 
-            }
-            //calculate percentage of result and replace in chain operation.
-            let percentage = this.operationChain.pop();
-            this.eval();
-            let result = this.result;
-            let percentValue = (result * percentage)/100;
-            this.operationChain.push(percentValue);
         },
 
         changeSign: function(){
-            if(this.numberEntered && this.number.length){
-                this.number[0] === '-' ? this.number.shift() : this.number.unshift('-');
+            if(this.result.length && !this.number.length){
+                this.result[0] = this.result[0]*-1;
+                this.display = this.result;
+            }else if(this.number.length){
+                if(this.number[0] === '-'){
+                    this.number.shift();
+                }else{
+                    this.number.unshift('-');
+                }
                 this.display = +this.number.join('');
-                this.updateDisplay();
-                console.log(this.number);
-            }else{
-                this.pushOperation('*');
-                this.operationChain.push('-1');
-                this.eval();
             }
-        },
-
-        eval: function(){
-            let numbers = [],
-                operands = [],
-                secondLast = this.operationChain.length - 2,
-                index = 0;
-                self = this;
-            /*
-            for(let i=0; i <= secondLast ; i++){
-                if( i % 2 ){
-                    operands.push(this.operationChain[i]);
-                }else{
-                    numbers.push(+this.operationChain[i])
-                }
-            };*/
-
-            this.operationChain.forEach( function(operand){
-                if( ['-','+','*','/','='].includes(operand) ){
-                    operands.push(operand);
-                }else{
-                    numbers.push(+operand);
-                }
-            });
-            //evaluate percentage first;
-            
-
-            //evaluate rest;
-            /*
-            self.result = numbers[index];
-            operands.forEach( function(operand) {
-                index++;
-                switch(operand){
-                    case '+':
-                        self.result += numbers[index];
-                        break;
-                    case '-':
-                        self.result -= numbers[index];
-                        break;
-                    case '*':
-                        self.result *= numbers[index];
-                        break;
-                    case '/':
-                        self.result /= numbers[index];
-                        break;
-                    case '=':
-                        self.result = self.result;
-                        break;
-                }
-                */
-            this.result = numbers[0];
-            for(let i=1; i < numbers.length; i++){
-                switch(operands[index]){
-                    case '+':
-                        self.result += numbers[i];
-                        break;
-                    case '-':
-                        self.result -= numbers[i];
-                        break;
-                    case '*':
-                        self.result *= numbers[i];
-                        break;
-                    case '/':
-                        self.result /= numbers[i];
-                        break;
-                    case '=':
-                        self.result = self.result;
-                        break;
-                }
-                index++;
-            };
-            this.result = this.result;
-            this.display = this.result;
             this.updateDisplay();
-            console.log('numbers = '+ numbers);
-            console.log('operand = '+operands);
-            console.log('result = '+this.result);
-        },
-        
-        percent: function(){
-            this.percentage();
         },
 
-        add: function(){
-            this.updateOperationChain('+');
-        },
+        execute: function(operand){
+            let number = 0;
+            if(this.number.length){
+                number = +this.number.join('');
+            }
+                
+            if(this.result.length){
 
-        multiply: function(){
-            this.updateOperationChain('*');
-        },
-
-        divide: function(){
-            this.updateOperationChain('/');
-        },
-
-        substract: function(){
-            this.updateOperationChain('-');
-        },
-
-        equal: function(){
-            this.updateOperationChain('=');
+                switch(this.operand){
+                    case '+':
+                        this.result[0] += number;
+                        break;
+                    case '-':
+                        this.result[0] -= number;
+                        break;
+                    case '*':
+                        this.result[0] *= number;
+                        break;
+                    case '/':
+                        this.result[0] /= number;
+                        break;
+                    case '=':
+                        this.result[0] = this.result[0];
+                        break;
+                }  
+            }else{
+                this.result.push(number);
+            }
+            this.display = this.result[0];
+            this.number = [];
+            this.updateDisplay();
+            this.operand = operand; 
         }
     }
-    
+
 
     $('.btn-digit').on('click', function (){
-         calc.concatDigit(this);
+        calc.concatDigit(this);
     });
 
-    $('#btn-sign').on('click', function (){
-        calc.changeSign();
+    $('#btn-decimal').on('click', function (){
+        calc.addDecimal();
+    });
+
+    $('#btn-CE').on('click', function (){
+        calc.clearNum();
+    });
+
+    $('#btn-C').on('click', function (){
+        calc.clearAll();
+    });
+
+    $('#btn-plus').on('click', function(){
+         calc.execute('+');
+    });
+
+    $('#btn-minus').on('click', function(){
+        calc.execute('-');
+    });
+
+    $('#btn-multiply').on('click', function(){
+        calc.execute('*');
+    });
+
+    $('#btn-divide').on('click', function(){
+        calc.execute('/');
+    });
+
+    $('#btn-equal').on('click', function(){
+        calc.execute('=');
     });
 
     $('#btn-percent').on('click', function(){
         calc.percent();
     });
 
-    $('#btn-plus').on('click', function(){
-        calc.add();
-    });
-
-    $('#btn-minus').on('click', function(){
-        calc.substract();
-    });
-
-    $('#btn-multiply').on('click', function(){
-        calc.multiply();
-    });
-
-    $('#btn-divide').on('click', function(){
-        calc.divide();
-    });
-
-    $('#btn-equal').on('click', function(){
-        calc.equal();
+    $('#btn-sign').on('click', function (){
+        calc.changeSign();
     });
 
 });
---
+++ js/calculator_old.js
@@ -0,0 +1,218 @@
+
+'use strict'
+
+$( function (){
+
+
+    let calc = {
+        display : '0',
+        number : [],
+        operationChain : [],
+        result : 0,
+        sign : 1,
+        numberEntered : true,
+
+        updateDisplay: function(){
+            $('.display').val(this.display);
+        },
+
+        concatDigit: function(btn){
+            this.numberEntered = true;
+            let digit = $(btn).val();
+            this.number.push(digit);
+            //this.number.length ? this.display = this.number.join('') : this.number.push('0');
+            this.display = this.number.join('');
+            this.updateDisplay();
+        },
+
+        pushNumber: function(){
+            if(!this.number.length){
+                this.number = ['0'];
+            }
+            let number = this.number.join('');
+            this.operationChain.push(number);
+            this.number = [];
+            this.numberEntered = false;
+        },
+
+        pushOperation: function(operand){
+            let lastIndex = this.operationChain.length - 1,
+            lastElem  = this.operationChain[lastIndex];
+            if( ['*','/','+','-','='].includes(lastElem) ){
+                this.operationChain[lastIndex] = operand;
+            }else{
+                this.operationChain.push(operand);
+            }
+        },
+
+        updateOperationChain: function(operand){
+            if(this.numberEntered){
+                this.pushNumber();
+            }
+            this.pushOperation(operand);
+            console.log('chain:'+this.operationChain);
+            this.eval();
+        },
+
+        percentage: function(){
+            let last = this.operationChain.length - 1;
+            if(['*','/','+','-','='].includes(this.operationChain[last])){
+                this.operationChain.pop();  //remove operation 
+            }
+            //calculate percentage of result and replace in chain operation.
+            let percentage = this.operationChain.pop();
+            this.eval();
+            let result = this.result;
+            let percentValue = (result * percentage)/100;
+            this.operationChain.push(percentValue);
+        },
+
+        changeSign: function(){
+            if(this.numberEntered && this.number.length){
+                this.number[0] === '-' ? this.number.shift() : this.number.unshift('-');
+                this.display = +this.number.join('');
+                this.updateDisplay();
+                console.log(this.number);
+            }else{
+                this.pushOperation('*');
+                this.operationChain.push('-1');
+                this.eval();
+            }
+        },
+
+        eval: function(){
+            let numbers = [],
+                operands = [],
+                secondLast = this.operationChain.length - 2,
+                index = 0;
+                self = this;
+            /*
+            for(let i=0; i <= secondLast ; i++){
+                if( i % 2 ){
+                    operands.push(this.operationChain[i]);
+                }else{
+                    numbers.push(+this.operationChain[i])
+                }
+            };*/
+
+            this.operationChain.forEach( function(operand){
+                if( ['-','+','*','/','='].includes(operand) ){
+                    operands.push(operand);
+                }else{
+                    numbers.push(+operand);
+                }
+            });
+            //evaluate percentage first;
+            
+
+            //evaluate rest;
+            /*
+            self.result = numbers[index];
+            operands.forEach( function(operand) {
+                index++;
+                switch(operand){
+                    case '+':
+                        self.result += numbers[index];
+                        break;
+                    case '-':
+                        self.result -= numbers[index];
+                        break;
+                    case '*':
+                        self.result *= numbers[index];
+                        break;
+                    case '/':
+                        self.result /= numbers[index];
+                        break;
+                    case '=':
+                        self.result = self.result;
+                        break;
+                }
+                */
+            this.result = numbers[0];
+            for(let i=1; i < numbers.length; i++){
+                switch(operands[index]){
+                    case '+':
+                        self.result += numbers[i];
+                        break;
+                    case '-':
+                        self.result -= numbers[i];
+                        break;
+                    case '*':
+                        self.result *= numbers[i];
+                        break;
+                    case '/':
+                        self.result /= numbers[i];
+                        break;
+                    case '=':
+                        self.result = self.result;
+                        break;
+                }
+                index++;
+            };
+            this.result = this.result;
+            this.display = this.result;
+            this.updateDisplay();
+            console.log('numbers = '+ numbers);
+            console.log('operand = '+operands);
+            console.log('result = '+this.result);
+        },
+        
+        percent: function(){
+            this.percentage();
+        },
+
+        add: function(){
+            this.updateOperationChain('+');
+        },
+
+        multiply: function(){
+            this.updateOperationChain('*');
+        },
+
+        divide: function(){
+            this.updateOperationChain('/');
+        },
+
+        substract: function(){
+            this.updateOperationChain('-');
+        },
+
+        equal: function(){
+            this.updateOperationChain('=');
+        }
+    }
+    
+
+    $('.btn-digit').on('click', function (){
+         calc.concatDigit(this);
+    });
+
+    $('#btn-sign').on('click', function (){
+        calc.changeSign();
+    });
+
+    $('#btn-percent').on('click', function(){
+        calc.percent();
+    });
+
+    $('#btn-plus').on('click', function(){
+        calc.add();
+    });
+
+    $('#btn-minus').on('click', function(){
+        calc.substract();
+    });
+
+    $('#btn-multiply').on('click', function(){
+        calc.multiply();
+    });
+
+    $('#btn-divide').on('click', function(){
+        calc.divide();
+    });
+
+    $('#btn-equal').on('click', function(){
+        calc.equal();
+    });
+
+});
